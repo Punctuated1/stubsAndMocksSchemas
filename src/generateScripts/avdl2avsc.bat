2d05d3c9163607d0e26f3c94e3bc7dbfc2a46257
Punctuated1 <105320226+Punctuated1@users.noreply.github.com>
2022-07-08 19:19:52 -0400

    Add files via upload

---
+++ src/generateScripts/avdl2avsc.bat
@@ -0,0 +1,5 @@
+d:
+cd D:\MyDocuments\workspaces\ConfigCoord\MyTestSchemaProject\src\avroprotocol\myapp\generated\avro\
+java -jar D:\Downloads\AvroJars\avro-1.10.2\avro-tools-1.10.2.jar idl2schemata Tweet.avdl 
+java -jar D:\Downloads\AvroJars\avro-1.10.2\avro-tools-1.10.2.jar idl2schemata TwitterEventFeedback.avdl 
+pause
---
+++ src/generateScripts/avsc2java.bat
@@ -0,0 +1,5 @@
+d:
+cd D:\MyDocuments\workspaces\ConfigCoord\MyTestSchemaProject\src
+java -jar D:\Downloads\AvroJars\avro-1.10.2\avro-tools-1.10.2.jar compile schema avroprotocol\myapp\generated\avro\TweetDto.avsc java
+java -jar D:\Downloads\AvroJars\avro-1.10.2\avro-tools-1.10.2.jar compile schema avroprotocol\myapp\generated\avro\TwitterEventFeedbackDto.avsc java
+pause
